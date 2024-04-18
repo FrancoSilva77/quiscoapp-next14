9bb528902da176f1b96680e695654134e3bdd946
Franco Silva <franco_tigres18@hotmail.com>
2024-04-17 21:28:39 -0600

    feat(Obtener productos por categoria)

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,9 +1,24 @@
-export default function OrderPage({
+import { prisma } from '@/src/lib/prisma';
+
+async function getProducts(category: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category,
+      },
+    },
+  });
+
+  return products;
+}
+
+export default async function OrderPage({
   params,
 }: {
   params: { category: string };
 }) {
-  console.log(params.category);
+  const products = await getProducts(params.category);
+  console.log(products);
 
   return <div>OrderPage</div>;
 }
