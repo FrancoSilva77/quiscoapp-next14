2e0fa7099bb1ee27cc0ba11fd299d989e4393cf7
Franco Silva <franco_tigres18@hotmail.com>
2024-04-17 21:55:51 -0600

    feat(Desing Products y Order)

---
+++ components/products/ProductCard.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import { formatCurrency } from '@/src/utils';
+import { Product } from '@prisma/client';
+
+type ProductCardProps = {
+  product: Product;
+};
+
+export default function ProductCard({ product }: ProductCardProps) {
+  return (
+    <>
+      <div className="border bg-white">
+        <Image
+          width={400}
+          height={400}
+          src={`/products/${product.image}.jpg`}
+          alt="Imagen producto"
+        />
+        <div className="p-5">
+          <h3 className="text-2xl font-bold">{product.name}</h3>
+          <p className="mt-5 font-black text-4xl text-amber-500">
+            {formatCurrency(product.price)}
+          </p>
+
+          <button
+            type="button"
+            className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+          >Agregar</button>
+        </div>
+      </div>
+    </>
+  );
+}
