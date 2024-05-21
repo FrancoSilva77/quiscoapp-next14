5050ac1e6ace9fa039149e6daf4fc20782f8f4b1
Franco Silva <franco_tigres18@hotmail.com>
2024-05-20 20:17:26 -0600

    feat(Listar Productos Admin)

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,20 @@
-import Heading from "@/components/ui/Heading";
+import ProductTable from '@/components/products/ProductTable';
+import Heading from '@/components/ui/Heading';
+import { prisma } from '@/src/lib/prisma';
 
-export default function ProductsPage() {
+async function getProducts() {
+  const products = await prisma.product.findMany();
+  return products;
+}
+
+export default async function ProductsPage() {
+  const products = await getProducts();
+
   return (
     <>
-    <Heading>Administrar Productos</Heading>
+      <Heading>Administrar Productos</Heading>
+
+      <ProductTable products={products}/>
     </>
   );
 }
