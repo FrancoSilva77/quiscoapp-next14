37d7f468435ef45ed17d2363d8f9b6894e2d9080
Franco Silva <franco_tigres18@hotmail.com>
2024-05-20 20:33:00 -0600

    feat(Logica Pagination)

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,8 +2,10 @@ import ProductTable from '@/components/products/ProductTable';
 import Heading from '@/components/ui/Heading';
 import { prisma } from '@/src/lib/prisma';
 
-async function getProducts() {
+async function getProducts(page: number, pageSize: number, skip: number) {
   const products = await prisma.product.findMany({
+    take: pageSize,
+    skip,
     include: {
       category: true,
     },
@@ -13,8 +15,16 @@ async function getProducts() {
 
 export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>;
 
-export default async function ProductsPage() {
-  const products = await getProducts();
+export default async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  const page = +searchParams.page || 1;
+  const pageSize = 10;
+  const skip = (page - 1) * pageSize;
+
+  const products = await getProducts(page, pageSize, skip);
 
   return (
     <>
