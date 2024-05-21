780630014e2f8fde6e3494c20a9d88c543de17a6
Franco Silva <franco_tigres18@hotmail.com>
2024-05-20 22:01:07 -0600

    feat(Numerar Paginacion)

--- components/products/ProductsNavigation.tsx
+++ components/products/ProductsNavigation.tsx
@@ -9,6 +9,8 @@ export default function ProductsNavigation({
   page,
   totalPages,
 }: ProductsNavigationProps) {
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
   return (
     <nav className="flex justify-center py-10">
       {page > 1 && (
@@ -19,6 +21,17 @@ export default function ProductsNavigation({
           &laquo;
         </Link>
       )}
+
+      {pages.map((currentPage) => (
+        <Link
+          href={`/admin/products?page=${page}`}
+          className={`${
+            page === currentPage && 'font-black'
+          } bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-9`}
+        >
+          {currentPage}
+        </Link>
+      ))}
       {page < totalPages && (
         <Link
           href={`/admin/products?page=${page + 1}`}
