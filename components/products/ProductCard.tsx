757bebe3347e8f03d5e2e760dfd0c04d177f85d1
Franco Silva <franco_tigres18@hotmail.com>
2024-06-03 10:03:02 -0600

    feat(Mostrar Imagen Cloudinary)

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { formatCurrency } from '@/src/utils';
+import { formatCurrency, getImagePath } from '@/src/utils';
 import { Product } from '@prisma/client';
 import AddProductButton from './AddProductButton';
 
@@ -8,13 +8,16 @@ type ProductCardProps = {
 };
 
 export default function ProductCard({ product }: ProductCardProps) {
+
+  const imagePath = getImagePath(product.image)
+  
   return (
     <>
       <div className="border bg-white">
         <Image
           width={400}
           height={400}
-          src={`/products/${product.image}.jpg`}
+          src={imagePath}
           alt="Imagen producto"
         />
         <div className="p-5">
